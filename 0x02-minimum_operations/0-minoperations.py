ab55e42d5b44b4ea24dfce9fb9029e460ebe5abd
mugishab2020 <mugishab2020@gmail.com>
2024-04-25 15:09:40 +0200

    We got this

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,8 +10,8 @@ def minOperations(n):
         return 0
     sum = 0
     i = 2
-    while(n > 1):
-        while(n % i != 0):
+    while (n > 1):
+        while (n % i != 0):
             i = i + 1
         sum += i
         n /= i
