cde9205a46394c02827a9c7d1a43855bc5d907ee
mugishab2020 <mugishab2020@gmail.com>
2024-05-09 12:07:48 +0200

    UTF-8 encoding verified

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,39 +5,44 @@ check the varidation of the UTF-8 encoding'''
 from typing import List
 
 
-def validUTF8(data: List[int]) -> bool:
-    '''Check the UTF-8 encoding and return True if the data is valid
-    else return False'''
-    if not isinstance(data, list) or\
-            not all([isinstance(num, int) for num in data]):
+def valid_utf8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+
+    :param data: List of integers representing the data set
+    :return: True if data is a valid UTF-8 encoding, else return False
+    """
+    if not isinstance(data, list) or \
+            not all(isinstance(num, int) for num in data):
         return False
+
     binary_list = [format(decimal_number % 256, '08b')
                    for decimal_number in data]
     i = 0
     while i < len(binary_list):
-        byt = binary_list[i]
-        if byt.startswith('0'):
-            i = i + 1
-        elif byt.startswith('110'):
+        byte = binary_list[i]
+        if byte.startswith('0'):
+            i += 1
+        elif byte.startswith('110'):
             if len(binary_list) < i + 2:
                 return False
             if not binary_list[i + 1].startswith('10'):
                 return False
-            i = i + 2
-        elif byt.startswith('1110'):
+            i += 2
+        elif byte.startswith('1110'):
             if len(binary_list) < i + 3:
                 return False
             for j in range(i + 1, i + 3):
                 if not binary_list[j].startswith('10'):
                     return False
-            i = i + 3
-        elif byt.startswith('11110'):
+            i += 3
+        elif byte.startswith('11110'):
             if len(binary_list) < i + 4:
                 return False
             for j in range(i + 1, i + 4):
                 if not binary_list[j].startswith('10'):
                     return False
-            i = i + 4
+            i += 4
         else:
             return False
     return True
