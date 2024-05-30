f7d98dcf86b4f598e63972523068bccc18f31aa2
mugishab2020 <mugishab2020@gmail.com>
2024-05-30 09:27:47 +0200

    We have the rotated matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,11 +3,21 @@
 
 
 def rotate_2d_matrix(matrix):
+    """
+      Rotate The 2 matrix
+      Args:
+          - matrix: list of two dimension array
+      Return:
+          - Nothing
+    """
+    # Get the number of rows and colu
 
     n = len(matrix)
-
+    # 1. Transpose the matrix
     for i in range(n):
-        for j in range(i, n):
+        for j in range(i+1, n):  # Avoid redundant swaps (i == j)
+            # Swap elements
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    # 2. Reverse each row finally
     for a in range(n):
         matrix[a].reverse()
