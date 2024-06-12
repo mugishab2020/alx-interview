77be8a493f2047c53529ce46e68119773807ddc9
mugishab2020 <mugishab2020@gmail.com>
2024-06-12 12:39:21 +0200

    Trying the island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,17 +3,20 @@
 
 
 def island_perimeter(grid):
-    '''This is the function to generate the island parameters'''
-    params = 0
-
+    '''
+    Island Perimeter
+    '''
+    perimeter = 0
     for i in range(len(grid)):
-        for j in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                params += 4
-                # Check the cell above
-                if i > 0 and grid[i][j] == grid[i-1][j]:
-                    params -= 2
-                # Check the cell to the left
-                if j > 0 and grid[i][j] == grid[i][j-1]:
-                    params -= 2
-    return params
+                perimeter += 4
+                if j - 1 >= 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j + 1 < len(grid[i]) and grid[i][j + 1] == 1:
+                    perimeter -= 1
+                if i + 1 < len(grid) and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if i - 1 >= 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+    return perimeter
